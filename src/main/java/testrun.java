9dc5f6e3b680bf67299a2f3f46bac25c95a75ba8
SterAllen <604712285@qq.com>
2019-10-11 22:40:44 +0800

    改了一下11111111111111111111111111111111111

--- src/main/java/testrun.java
+++ src/main/java/testrun.java
@@ -1,5 +1,5 @@
 public class testrun {
     public static void main(String[] args) {
-        System.out.println("测试 新建一个maven项目从idea上传至git");
+        System.out.println("测试  新建一个maven项目从idea上传至git");
     }
 }
