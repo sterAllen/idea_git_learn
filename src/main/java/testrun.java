3cfa141e0d329b3196c709cac615f8fc0f3d34aa
SterAllen <604712285@qq.com>
2019-10-12 10:50:48 +0800

    1111111111111111111111111

--- src/main/java/testrun.java
+++ src/main/java/testrun.java
@@ -1,5 +1,5 @@
 public class testrun {
     public static void main(String[] args) {
-        System.out.println("测新          daw             建一个maven项目从idea上传至git");
+        System.out.println("一个maven项目从idea上传至git");
     }
 }
